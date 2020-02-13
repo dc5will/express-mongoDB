5c587db9816b7277b695bc219d2c1c3a0d6ad089
William Y Wong <wwong1987@gmail.com>
2020-02-12 22:04:39 -0800

    linked up mongoDB to project

--- app.js
+++ app.js
@@ -1,12 +1,27 @@
-'use strict';
+"use strict";
 
-const express = require('express');
+const express = require("express");
 const app = express();
+const MongoClient = require("mongodb").MongoClient;
 
 app.listen(8000, function() {
-  console.log('Express server is listening on http://localhost:8000');
-})
+  console.log("Express server is listening on http://localhost:8000");
+});
 
-app.get('/', (req, res) => {
-  res.send('Hello there!');
-})
+app.get("/", (req, res) => {
+  res.send("Hello there!");
+});
+
+let ObjectID = require("mongodb").ObjectID; // we will use this later
+
+MongoClient.connect(
+  "mongodb+srv://will:Password123!@cluster0-1ksk9.mongodb.net/test?retryWrites=true&w=majority",
+  { useNewUrlParser: true, useUnifiedTopology: true }
+);
+(err, db) => {
+  let dbase = db.db("crud");
+  if (err) return console.log(err);
+  app.listen(3000, () => {
+    console.log("app working on 3000");
+  });
+};
